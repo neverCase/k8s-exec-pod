8ceec7842d465ebedc0cf588f7fdeeb95784c5d7
neverCase <1024769485@qq.com>
2020-09-09 18:14:42 +0800

    init code

---
+++ exec.go
@@ -0,0 +1,44 @@
+package k8s_exec_pod
+
+import (
+	"io"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog"
+)
+
+// ExecOptions passed to ExecWithOptions
+type ExecOptions struct {
+	Command       []string
+	Namespace     string
+	PodName       string
+	ContainerName string
+	Stdin         io.Reader
+	CaptureStdout bool
+	CaptureStderr bool
+	// If false, whitespace in std{err,out} will be removed.
+	PreserveWhitespace bool
+}
+
+func EstablishSSH(masterUrl, kubeconfigPath, podName, namespace string) error {
+	cfg, err := clientcmd.BuildConfigFromFlags(masterUrl, kubeconfigPath)
+	if err != nil {
+		klog.Fatalf("Error building kubeconfig: %s", err.Error())
+	}
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
+	}
+	req := kubeClient.CoreV1().RESTClient().Post().
+		Resource("pods").
+		Name(podName).
+		Namespace(namespace).
+		SubResource("exec")
+	scheme := runtime.NewScheme()
+	if err := corev1.AddToScheme(scheme); err != nil {
+		klog.Fatalf("error adding to scheme: %v", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/nevercase/k8s-exec-pod
+
+go 1.14
+
+require (
+	k8s.io/api v0.17.3
+    k8s.io/apimachinery v0.17.3
+    k8s.io/client-go v0.0.0-20190918160344-1fbdaa4c8d90
+    k8s.io/klog v1.0.0
+)
